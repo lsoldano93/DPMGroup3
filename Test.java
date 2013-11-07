0305c508c4d6e4b5f81fc33371a9afbb1d3b9390
Tuan-Anh Nguyen <tuan-anh.nguyen@mail.mcgill.ca>
2013-11-07 14:11:41 -0500

    Fixed main method in main class to include test class

--- Test.java
+++ Test.java
@@ -1,46 +1,75 @@
 import lejos.nxt.*;
 import lejos.nxt.comm.RConsole;
 
-
 /**
- * This is a test class. It tests the ultrasonic sensors and color sensors. It will return
- * the results through the console viewer provided by NXJ. (Connection is through USB)
+ * This is a test class. It tests the ultrasonic sensors and color sensors. It
+ * will return the results through the console viewer provided by NXJ.
+ * (Connection is through USB)
+ * 
  * @author Team-03
- *
+ * 
  */
 public class Test {
+	
+	private Dashboard dashboard;
+	private ColorSensor leftColor;
+	private ColorSensor rightColor;
+	private Odometer odometer;
+	private OdometryCorrection correction;
+	private UltrasonicSensor usHigh;
+	private UltrasonicSensor usLow;
+	private UltrasonicSensorFilter usFilterHigh;
+	private UltrasonicSensorFilter usFilterLow;
+	private Navigation nav;
+	private USLocalizer localizer;
 
 	/**
-	 * Main method of the class. It will either call the method to test the ultrasonic sensor 
-	 * color sensor
-	 * 
+	 * Constructor for this test class. It will constrcut all necessary objects
 	 */
-//	public static void main(String[] args) {
-//		Button.waitForAnyPress();
-//		lightTest();
-//		
-//
-//	}
-	public Test(){
-		
+	public Test() {
+		// Dashboard instantiation
+		NXTRegulatedMotor leftMotor = Motor.A;
+		NXTRegulatedMotor rightMotor = Motor.B;
+		dashboard = new Dashboard(leftMotor, rightMotor);
+
+		// Odometer & Odoemetry Correction instantiation
+		leftColor = new ColorSensor(SensorPort.S3);
+		rightColor = new ColorSensor(SensorPort.S4);
+		odometer = new Odometer(leftMotor, rightMotor, 20, true);
+		correction = new OdometryCorrection(leftColor,	rightColor, odometer);
+		correction.start();
+
+		// Ultrasonic Filter instantiation
+		usHigh = new UltrasonicSensor(SensorPort.S1);
+		usLow = new UltrasonicSensor(SensorPort.S2);
+		usFilterHigh = new UltrasonicSensorFilter(usHigh, 20, true);
+		usFilterLow = new UltrasonicSensorFilter(usLow,	20, true);
+
+		// Navigation instantiation
+		nav = new Navigation(dashboard, odometer, usFilterHigh);
+
+		// Localition instantiation (but not actually perforing localization
+		// yet)
+		localizer = new USLocalizer(odometer, usHigh, USLocalizer.LocalizationType.FALLING_EDGE, nav);
+
 	}
 
-	// This test has the robot drive straight to a block with the ultrasonic sensor facing it.
-	// It will stop when the ultrasonic reading reads 5cm. 
+	// This test has the robot drive straight to a block with the ultrasonic
+	// sensor facing it.
+	// It will stop when the ultrasonic reading reads 5cm.
 	/**
-	 * This is the ultrasonic sensor test. This test has the robot drive straight 
-	 * to a block with the ultrasonic sensor facing it.
+	 * This is the ultrasonic sensor test. This test has the robot drive
+	 * straight to a block with the ultrasonic sensor facing it.
 	 */
 	public void usTest() {
-
-		UltrasonicSensor us = new UltrasonicSensor(SensorPort.S1);
-		UltrasonicSensorFilter ultra = new UltrasonicSensorFilter(us, 60, true);
+		Button.waitForAnyPress();
 		RConsole.open();
 		RConsole.println("Connected");
 		Button.waitForAnyPress();
-		int distance = ultra.getFilteredDistance(); // The ultrasonic sensor pings every 60ms
-		while (distance > 5) { 
-			distance = ultra.getFilteredDistance();
+		int distance = usFilterHigh.getFilteredDistance(); // The ultrasonic sensor
+													// pings every 60ms
+		while (distance > 5) {
+			distance = usFilterHigh.getFilteredDistance();
 			RConsole.println("" + distance);
 		}
 
@@ -48,28 +77,27 @@ public class Test {
 
 	}
 
-	// This test has the robot drive straight for 150 cm while having the color sensor facing
+	// This test has the robot drive straight for 150 cm while having the color
+	// sensor facing
 	// directly downwards. Light readings were taken every 80ms.
 	/**
-	 * Performs a test on the color sensor. This test has the robot drive straight for 150 cm 
-	 * while having the color sensor facing downwards.
+	 * Performs a test on the color sensor. This test has the robot drive
+	 * straight for 150 cm while having the color sensor facing downwards.
 	 */
 	public void lightTest() {
 
-		ColorSensor cs = new ColorSensor(SensorPort.S2);
-		cs.setFloodlight(true);
+		leftColor.setFloodlight(true);
 		RConsole.open();
 		RConsole.println("Connected");
 		Button.waitForAnyPress();
-		Dashboard dashboard = new Dashboard(Motor.A, Motor.B);
 		dashboard.goForward(150, true);
-		while(dashboard.isMotorRotating()) {
+		while (dashboard.isMotorRotating()) {
 			// The color sensor will give a reading every 80ms
-			try{ 
+			try {
 				Thread.sleep(80);
+			} catch (Exception e) {
 			}
-			catch(Exception e){}
-			int color = cs.getNormalizedLightValue();
+			int color = leftColor.getNormalizedLightValue();
 			RConsole.println("" + color);
 
 		}
